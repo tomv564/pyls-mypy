bf12dc95b5336f9f7379c7e2c0b95cc4c66ef0e3
Richardk2n <Richard1.Kellnberger@web.de>
2020-07-10 10:26:31 +0200

    typo

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from setuptools import setup
-from pyls_mypy import _version
+from mypy_ls import _version
 
 if __name__ == "__main__":
     setup(version=_version.__version__)
